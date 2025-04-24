41f6cf183af3f47550b44454a9469d21a4e55c41
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2025-04-24 11:44:23 +0300

    ui (notes): List notes and notes card draggable done

---
+++ src/components/notes/colorPalettes.ts
@@ -0,0 +1,22 @@
+export const colorPalettes = {
+    cyan: {
+        cardBg: "bg-cyan-300",
+        articleBg: "bg-cyan-200",
+    },
+    green: {
+        cardBg: "bg-green-300",
+        articleBg: "bg-green-200",
+    },
+    red: {
+        cardBg: "bg-red-300",
+        articleBg: "bg-red-200",
+    },
+    orange: {
+        cardBg: "bg-orange-300",
+        articleBg: "bg-orange-200",
+    },
+    yellow: {
+        cardBg: "bg-yellow-300",
+        articleBg: "bg-yellow-200",
+    },
+};
