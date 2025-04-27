a4af53d9d2b450ebf9276fcfbaf10ce18dac6ad1
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2025-04-27 17:14:35 +0300

    refactor (note): refactor function in props drilling. let's use context in my custom hooks

--- src/hooks/notes/useWaitUpdate.ts
+++ src/hooks/notes/useWaitUpdate.ts
@@ -1,23 +1,35 @@
-import { NotePayload } from "@/types/notePayload";
-import { useCallback, useRef } from "react";
+import { useNotes } from "@/context/NotesContext";
+import { NoteFrontEnd } from "@/types/noteFrontend";
+import { useCallback, useRef, useState } from "react";
 
+/**
+ * Hooks to wait for a certain time before updating the note, we use it to prevent database overload
+ * when the user is typing, we wait for 2 seconds before updating the note
+ * @param note note to be updated 
+ * @param timeout time to wait before updating the note
+ * @param timeout default is 2 seconds
+ * @returns the loading state and the function waitAndUpdate
+ */
 export const useWaitUpdate = (
-    payload: NotePayload,
-    updateNote: (note: NotePayload) => void,
+    note: NoteFrontEnd,
     timeout: number = 2
 ) => {
     const timerRef = useRef<NodeJS.Timeout | undefined>(undefined);
+    const [loading, setLoading] = useState(false);
+    const { updateNote } = useNotes();
 
-    const waitAndUpdate = useCallback(() => {
+    const waitAndUpdate = useCallback(async () => {
+        setLoading(true);
         if (timerRef.current) {
             clearTimeout(timerRef.current);
         }
         const idTimer = setTimeout(() => {
-            updateNote(payload);
+            updateNote(note.id, note);
             timerRef.current = undefined;
+            setLoading(false);
         }, timeout * 1000);
         timerRef.current = idTimer;
-    }, [payload, updateNote, timeout]);
+    }, [updateNote, timeout, note]);
 
-    return waitAndUpdate;
+    return { loading, waitAndUpdate };
 }
