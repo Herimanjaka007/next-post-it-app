e3cbe87fee11cd15dc1ded3312e0aa7692f2576f
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2025-04-27 22:45:44 +0300

    feat (notes): create notes with choiced color done

--- src/components/notes/palette/PaletteItem.tsx
+++ src/components/notes/palette/PaletteItem.tsx
@@ -1,13 +1,17 @@
+import { Color } from "@/types/Color";
+
 interface PaletteItemProps {
+    color: Color
     colorClass?: string;
-    onClick?: () => void;
+    onClick: () => void;
+    active: boolean
 }
 
-const PaletteItem = ({ colorClass = "bg-cyan-500", onClick, children }: React.PropsWithChildren<PaletteItemProps>) => {
+const PaletteItem = ({ active, colorClass = "bg-cyan-500", onClick, children }: React.PropsWithChildren<PaletteItemProps>) => {
     return (
         <button
             type="button"
-            className={`${colorClass} w-8 h-8 rounded-full cursor-pointer border border-gray-300 hover:ring-2 hover:ring-offset-1 hover:ring-cyan-400 transition`}
+            className={`${colorClass} w-8 h-8 rounded-full cursor-pointer border border-gray-300 ${active && "ring-2 ring-offset-1 ring-cyan-400"} hover:ring-2 hover:ring-offset-1 hover:ring-cyan-400 transition`}
             onClick={onClick}
             aria-label="Select color"
         >
